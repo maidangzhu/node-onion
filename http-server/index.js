e28196ef0f9309a7f635a953ab2093522a0e660e
朱建业 <borisdunk@sina.com>
2023-04-03 17:36:28 +0800

    feat: 服务端渲染功能

--- http-server/index.js
+++ http-server/index.js
@@ -1,7 +1,9 @@
+const fs = require('fs')
 const Server = require('./lib/server.js')
 const Router = require('./lib/middleware/router.js')
+const handlebars = require('handlebars')
 
-const params = require("./aspect/params.js");
+const params = require('./aspect/params.js')
 
 const app = new Server()
 const router = new Router()
@@ -24,25 +26,55 @@ app.use(({ req }, next) => {
 
 app.use(params)
 
+// app.use(router.get('/coronavirus/index', async ({ route, res }, next) => {
+//   const { getCoronavirusKeyIndex } = require('./lib/module/mock')
+//   const index = getCoronavirusKeyIndex()
+//   res.setHeader('Content-Type', 'application/json')
+//   res.body = { data: index }
+//   await next()
+// }))
+
+// app.use(router.get('/coronavirus/:date', async ({ route, res }, next) => {
+//   const { getCoronavirusByDate } = require('./lib/module/mock')
+//   const data = getCoronavirusByDate(route.date)
+//   res.setHeader('Content-Type', 'application/json')
+//   res.body = { data }
+//   await next()
+// }))
+
 app.use(router.get('/coronavirus/index', async ({ route, res }, next) => {
   const { getCoronavirusKeyIndex } = require('./lib/module/mock')
   const index = getCoronavirusKeyIndex()
-  res.setHeader('Content-Type', 'application/json')
-  res.body = { data: index }
+
+  // 获取模板文件
+  const tpl = fs.readFileSync('./view/coronavirus_index.html', { encoding: 'utf-8' })
+
+  // 编译模板
+  const template = handlebars.compile(tpl)
+
+  // 将数据和模板结合
+  const result = template({ data: index })
+  res.setHeader('Content-Type', 'text/html')
+  res.body = result
   await next()
 }))
 
-app.use(router.get('/coronavirus/:date', async ({ route, res }, next) => {
+app.use(router.get('/coronavirus/:date', async ({ params, route, res }, next) => {
   const { getCoronavirusByDate } = require('./lib/module/mock')
   const data = getCoronavirusByDate(route.date)
-  res.setHeader('Content-Type', 'application/json')
-  res.body = { data }
-  await next()
-}))
 
-app.use(router.all('.*', async ({ params, req, res }, next) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.body = '<h1>Not Found</h1>'
-  res.statusCode = 404
+  if (params.type === 'json') {
+    res.setHeader('Content-Type', 'application/json')
+    res.body = { data }
+  } else {
+    const tpl = fs.readFileSync('./view/coronavirus_date.html', { encoding: 'utf-8' })
+
+    const template = handlebars.compile(tpl)
+    const result = template({ data })
+
+    res.setHeader('Content-Type', 'text/html')
+    res.body = result
+  }
+
   await next()
 }))
